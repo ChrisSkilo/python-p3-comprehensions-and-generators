d4933aca57ac067f5113a97106c56adc7c38c3c6
Chrispus <skilochris@gmail.com>
2023-12-11 11:07:46 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,10 @@
 
 def return_evens(num_list):
     pass
+    even_numbers = [num for num in num_list if num % 2 == 0]
+    return even_numbers
 
 def make_exclamation(sentence_list):
     pass
+    exclamation_sentences = [sentence + "!" for sentence in sentence_list]
+    return exclamation_sentences
